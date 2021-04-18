671de60d720d82dd13c7a6d62d2041f4973513cd
warblo001 <67009988+warblo001@users.noreply.github.com>
2021-04-18 17:32:54 +0200

    Configurate routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
+  resources :friendships, only: [:destroy]
+
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :friendships, only: [:create, :approve, :pending, :destroy]
+    get '/approve_friendship', to: 'friendship#approve'
+    get '/send_request_friendship', to: 'friendship#pending'
+  end
+
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
