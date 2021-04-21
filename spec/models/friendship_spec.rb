d2eeac4006f7d9ad6106b8e183541f17bf9dca2b
warblo001 <67009988+warblo001@users.noreply.github.com>
2021-04-21 14:09:55 +0200

    Fix error in user methods, add Rspec Tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Sign in page', type: :system do
+  describe 'index page' do
+    it 'shows sign in ' do
+      visit users_path
+      expect(page).to have_content('Sign in')
+    end
+    it 'shows Email ' do
+      visit users_path
+      expect(page).to have_content('Email')
+    end
+    it 'shows Password ' do
+      visit users_path
+      expect(page).to have_content('Password')
+    end
+    it 'shows Sign up ' do
+      visit users_path
+      expect(page).to have_content('Sign up')
+    end
+  end
 end
