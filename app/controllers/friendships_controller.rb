c74b167e0b522670c604fffb0ff9fa207c9cdc7f
warblo001 <67009988+warblo001@users.noreply.github.com>
2021-04-23 10:02:35 +0200

    Fix Post controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,23 +6,34 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(friend_id: params[:user_id])
     if @friendship.save
-      redirect_to users_path, notice: 'Friend request sent!'
+      redirect_to users_path
     else
-      redirect_to users_path, notice: 'Error! Try again'
+      redirect_to users_path, notice: 'Unable to save friend request, try again at a later time'
     end
   end
 
   def destroy
     @user = User.find(params[:id])
-    @friendship = current_user.inverse_friendships.find { |fship| fship.user == @user }
+    @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
     @friendship.destroy
-    redirect_to users_path, notice: "Removed #{@user.name} from friends"
+    redirect_to users_path, notice: "Removed #{@user.name} as friend"
   end
 
   def approve
     @user = User.find(params[:user_id])
-    @friendship = current_user.inverse_friendships.find { |fship| fship.user == @user }
+    @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
     @friendship.confirmed = true
-    redirect_to users_path(params[:user_id]), notice: "You're now friends with #{@user.name}!" if @friendship.save
+
+    return unless @friendship.save
+
+    current_user.friendships.create!(friend_id: @user.id, confirmed: true)
+
+    redirect_to users_path(params[:user_id]), notice: "Friendship with #{@user.name} confirmed"
+  end
+
+  private
+
+  def user
+    @friend = User.find(params[:user_id])
   end
 end
